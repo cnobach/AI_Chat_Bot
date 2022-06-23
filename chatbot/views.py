a2a1c8473d6f2c9813841f52ab334a39f74923c5
cnobach <clnobach@gmail.com>
2022-06-23 09:16:50 -0700

    Added basic template. Not sure how to get the bot working yet.

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the chatbot index")
+    template = loader.get_template('chatbot/index.html')
+    context = {
+        'heading': 'AI Chat Bot'
+    }
+    return render(request, 'chatbot/index.html', context)
